47a479532de261de09133e62a3b1b04c8d590f87
David Navarro <navarro.david.j@gmail.com>
2019-10-01 15:29:44 -0700

    Update demo pages to use dummy JSON schema data

--- src/pages/demo-pages.js
+++ src/pages/demo-pages.js
@@ -1,36 +1,49 @@
 import React, { useState } from "react";
 import Form from "react-jsonschema-form";
-import Grid from 'react-json-grid';
+import Grid from "react-json-grid";
 // import schema from '../jsonUI/organization/organization';
 
-const JSONschema = require("../json/JSONschema.json");
+const JSONschema = require("../jsonUI/organization/JSONSchema.json");
 const UIschema = require("../json/UIschema.json");
 
 export const Index = () => {
-
     const data = [
-        { 
-            name: 'hello', 
-            description: 'world',
-            sapID: '100',
-            salesforceID: '200',
-            administrators: ['admin1', 'admin2'],
-            active: true,
-            fleets: 'fleet1',
-
+        {
+            name: "ACME Fleet",
+            description: "ACME's Fleet",
+            sapId: "SAP2323455",
+            salesforceId: "SFID344",
+            address: {
+                addressLine1: "244 Acme Dr",
+                addressLine2: "Suite 3",
+                city: "Pleasanton",
+                state: "CA",
+                country: "USA"
+            }
         },
-        
+        {
+            name: "Marvel Fleet 2",
+            description: "Marvel's Fleet",
+            sapId: "SAP223433455",
+            salesforceId: "SFID344533",
+            address: {
+                addressLine1: "244 Acme Dr",
+                addressLine2: "Suite 3",
+                city: "Pleasanton",
+                state: "CA",
+                country: "USA"
+            }
+        }
     ];
 
     return (
         <div>
             <h1>Index</h1>
             <p>Demo of react-router-dom</p>
-            <p>Enter <code>http://localhost:3000/organization</code> into URL</p>
-            <Grid 
-                data={data}
-                style={{color: 'black', width: '100%'}}
-            />
+            <p>
+                Enter <code>http://localhost:3000/organization</code> into URL
+            </p>
+            <Grid data={data} style={{ color: "black", width: "100%" }} />
         </div>
     );
 };
@@ -43,7 +56,10 @@ export const Organization = () => {
     return (
         <div>
             <h1>Organization</h1>
-            <p>Great! No if you go to <code>http://localhost:3000</code>, you should see the index page again</p>
+            <p>
+                Great! Now if you go to <code>http://localhost:3000</code>, you
+                should see the index page again
+            </p>
             <Form
                 schema={JSONschema}
                 uiSchema={UIschema}
