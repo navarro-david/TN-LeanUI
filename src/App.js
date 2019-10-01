af9b6d0a9d748f42c5c782c5c5a3363dafd79717
David Navarro <navarro.david.j@gmail.com>
2019-10-01 12:31:38 -0700

    Added react-router-dom to show multiple pages in the single page app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import Form from "react-jsonschema-form";
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-const JSONschema = require('./json/JSONschema.json')
-const UIschema = require('./json/UIschema.json')
+import * as Page from './pages/demo-pages'
+
+import "./App.css";
+
 
 
 const App = () => {
+    
 
-  const handleOnSubmit = (type) => {
-    console.log('Place API call here to push submitted data', type);
-  }
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Form 
-          schema={JSONschema}
-          uiSchema={UIschema}
-          onSubmit={(type) => handleOnSubmit(type)}
-        />
-      </header>
-    </div>
-  );
-}
+    return (
+      <Router>
+        <div className="App">
+            <header className="App-header">
+                <div>
+                    <Switch>
+                        <Route path="/" exact component={Page.Index} />
+                        <Route
+                            path="/organization"
+                            component={Page.Organization}
+                        />
+                    </Switch>
+                </div>
+                {/* <Form
+                    schema={JSONschema}
+                    uiSchema={UIschema}
+                    onSubmit={type => handleOnSubmit(type)}
+                /> */}
+            </header>
+        </div>
+      </Router>
+    );
+};
 
 export default App;
