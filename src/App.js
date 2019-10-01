61383a1e1ead8336f36b404d6b1a0c5da3192fff
David Navarro <navarro.david.j@gmail.com>
2019-10-01 12:11:51 -0700

    Add UIschema.json file

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import logo from './logo.svg';
 import Form from "react-jsonschema-form";
 import './App.css';
 
-const schema = require('./json/JSONschema.json')
+const JSONschema = require('./json/JSONschema.json')
+const UIschema = require('./json/UIschema.json')
 
-const log = (type) => console.log.bind(console, type);
 
-const handleOnSubmit = () => {
-  log('Place API call here to push submitted data')
-}
+const App = () => {
 
-function App() {
+  const handleOnSubmit = (type) => {
+    console.log('Place API call here to push submitted data', type);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <Form schema={schema}
-          onSubmit={() => this.handleOnSubmit()}
+        <Form 
+          schema={JSONschema}
+          uiSchema={UIschema}
+          onSubmit={(type) => handleOnSubmit(type)}
         />
       </header>
     </div>
