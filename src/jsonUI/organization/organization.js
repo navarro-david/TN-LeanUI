285ab510c6236dfbb1ee98120101d54462f7d19d
Boban Jose <Boban.Jose@teletracnavman.com>
2019-10-01 15:06:19 -0700

    Organization Schema

---
+++ src/jsonUI/organization/organization.js
@@ -0,0 +1,9 @@
+const schema = require("../organization/JSONSchema.json");
+const uiSchema = require("../organization/UISchema.json")
+  
+export default {
+  schema,
+  uiSchema,
+  formData: {
+  },
+};
