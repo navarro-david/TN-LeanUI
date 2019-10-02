2e704b82506f1b87436b061a339a36271bc5c994
David Navarro <navarro.david.j@gmail.com>
2019-10-01 17:23:45 -0700

    Add dummy demo API calls

--- src/pages/demo-pages.js
+++ src/pages/demo-pages.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { withRouter } from 'react-router-dom';
 import Form from "react-jsonschema-form";
 import Button from '@material-ui/core/Button';
 
 import OrgGrid from '../components/OrganizationGrid'
-import { flexbox } from "@material-ui/system";
+
 
 // import schema from '../jsonUI/organization/organization';
 
@@ -65,13 +65,25 @@ export const Index = withRouter(({ history }) => {
 })
 
 export const EditOrganization = () => {
+
+    const [isSending, setIsSending] = useState(false)
+
+    // Place API call to add to API here
     const handleOnSubmit = type => {
         console.log("Place API call here to push submitted data", type);
+        if(isSending) return;
+        setIsSending(true)
+
+        // Dummy code
+        console.log('fetching from API...')
+        setTimeout(() => {
+            console.log('done fetching from API!')
+            setIsSending(false)
+        }, 5000)
     };
 
     return (
         <div>
-            <h1>Edit</h1>
             <Form
                 schema={JSONschema}
                 uiSchema={UIschema}
@@ -82,13 +94,25 @@ export const EditOrganization = () => {
 };
 
 export const AddOrganization = () => {
+    
+    const [isSending, setIsSending] = useState(false)
+
+    // Place API call to add to API here
     const handleOnSubmit = type => {
         console.log("Place API call here to push submitted data", type);
+        if(isSending) return;
+        setIsSending(true)
+
+        // Dummy code
+        console.log('fetching from API...')
+        setTimeout(() => {
+            console.log('done fetching from API!')
+            setIsSending(false)
+        }, 5000)
     };
 
     return (
         <div>
-            <h1>Edit</h1>
             <Form
                 schema={AddOrgSchema}
                 uiSchema={UIschema}
